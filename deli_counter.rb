789268085734bcb73b81c32c29a8495cebd88093
Stela Capsa <stela.capsa@gmail.com>
2019-11-18 16:41:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-def deli_counter(katz_deli,other_deli another_deli)
+def deli_counter(katz_deli,other_deli, another_deli)
    katz_deli = [] 
    other_deli = ["Logan", "Avi", "Spencer"] 
    another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
