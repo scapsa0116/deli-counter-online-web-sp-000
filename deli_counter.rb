d56e31630f33e262284d89079e1207601d87f794
Stela Capsa <stela.capsa@gmail.com>
2019-11-18 16:49:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,17 @@
-var katzDeli = [];
+katz_deli = []
 
-function currentLine(line){
-  if(!line.length) {
-    return "The line is currently empty.";
-  }
+def curent_line(line)
+  if(!line.length) 
+    return "The line is currently empty."
+  
   var lineNamesandNumbers = [];
   
-  for(var i=0; i<line.length; i++) {
+  for(var i=0; i<line.length; i++) 
     lineNamesandNumbers.push(i+1 + ". "+ line[i]);
-  }
+  
   console.log("The line is currently: " + lineNamesandNumbers)
   return "The line is currently: " + lineNamesandNumbers.join(', ');
-}
+
 
 function nowServing(line) {
   if(!line.length) {
