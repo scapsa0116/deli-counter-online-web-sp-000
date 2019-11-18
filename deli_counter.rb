fc32b034b91a45112018fd977ddf214bd5eb37e7
Stela Capsa <stela.capsa@gmail.com>
2019-11-18 16:30:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,19 @@
 
+katz_deli = []
 
-
-puts "The line is currently empty."
-
-def take_a_number(katz_deli, name)
+def take_a_number(katz_deli,index)
+  ary = [ ]
+  katz_deli.each_with_index do |name, index+1|
+    return ary << "H"
+  
   
 end
 
 def line(katz_deli)
+  ary1 = [ ]
+  katz_deli.each do |index +1,people|
+    return ary1 << "Now serving #{people}"
+  puts "The line is currently empty."
 end
 
 def now_serving(name) 
