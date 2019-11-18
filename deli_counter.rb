f4c494a86b748385cc48bb8383c33064a32f358a
Stela Capsa <stela.capsa@gmail.com>
2019-11-18 16:44:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,41 @@
-def deli_counter(katz_deli,other_deli, another_deli)
-   katz_deli = [] 
-   other_deli = ["Logan", "Avi", "Spencer"] 
-   another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+def katzDeli = [];
 
-end
+function currentLine(line){
+  if(!line.length) {
+    return "The line is currently empty.";
+  }
+  var lineNamesandNumbers = [];
+  
+  for(var i=0; i<line.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  }
+  console.log("The line is currently: " + lineNamesandNumbers)
+  return "The line is currently: " + lineNamesandNumbers.join(', ');
+}
 
-def take_a_number(katz_deli,index)
-  ary = [ ]
-  katz_deli.each_with_index do |name, index+1|
-    return ary << "Welcome, #{name}. You are number #{index+1} in line."
-  end
-  return ary
-end
+function nowServing(line) {
+  if(!line.length) {
+    console.log("There is nobody waiting to be served!")
+    return "There is nobody waiting to be served!"
+  } else {
+    //console.log("Currently serving " + line.shift());
+    return "Currently serving " + line.shift();
+  }
+}
 
-def line(katz_deli)
-  ary1 = [ ]
-  katz_deli.each do |index +1,people|
-    return ary1 << "Now serving #{people}"
-  puts "The line is currently empty."
-end
-
-def now_serving(name) 
-  puts "the next person in line"
-  puts"There is nobody waiting to be served!"
+function takeANumber(line, name){
+  line.push(name);
+  
+  console.log("Welcome, " + name + ". You are number " + line.length + " in line.");
+  
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+}
+takeANumber(katzDeli, "Ada")
+takeANumber(katzDeli, "Grace")
+takeANumber(katzDeli, "Kent")
+currentLine(katzDeli);
+nowServing(katzDeli);
+takeANumber(katzDeli, "Matz"); 
+currentLine(katzDeli); 
+nowServing(katzDeli);
+currentLine(katzDeli)
